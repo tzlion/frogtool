75f50faa71bd00149dc2104150954a654a7644e7
taizou <cactusgunman@gmail.com>
2023-04-23 00:05:19 +0100

    update build script again

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+set ver=0.1.0
 rem build script for the distributable versions of frogtool
 if not exist "venv\" (
     py -m venv venv
@@ -7,11 +8,12 @@ if not exist "venv\Lib\site-packages\PyInstaller" (
     venv\Scripts\python -m pip install pyinstaller
 )
 venv\Scripts\python -m PyInstaller frogtool.py  -F --version-file versioninfo --icon frog.ico
-copy README.md "dist\readme.txt"
+copy README.md "dist\readme.md"
+rem extremely dirty markdown to txt conversion by stripping out a few non-obvious characters
+py -c "open('dist/readme.txt','w').write(open('dist/readme.md','r').read().replace('`','').replace('### ',''))"
 copy LICENSE "dist\license.txt"
 copy frogtool.py "dist\frogtool.py"
 cd dist
-tar -a -cf frogtool-win.zip frogtool.exe readme.txt license.txt
-tar -a -cf frogtool-py.zip frogtool.py readme.txt license.txt
+tar -a -cf frogtool-%ver%-win.zip frogtool.exe readme.txt license.txt
+tar -a -cf frogtool-%ver%-py.zip frogtool.py readme.txt license.txt
 cd ../
-
