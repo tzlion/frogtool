860d25e0ee86cf57a5979b296986354cb45ed2b5
Kevin Schellenberg <wikkiewikkie@gmail.com>
2023-07-28 11:30:31 -0400

    add dialog to preview and confirm boot image replacement

--- tadpole.py
+++ tadpole.py
@@ -168,6 +168,132 @@ def BGM_change(source=""):
         msgBox.close()
         QMessageBox.about(window,"Failure", "Something went wrong while trying to change the background music")
 
+
+class BootLogoViewer(QLabel):
+    """
+    Args:
+        parent (BootConfirmDialog): Parent widget. Used to enable/disable controls on parent.
+        changeable (bool): If True, will allow importing new image. If False, will just allow static display.
+    """
+    def __init__(self, parent, changeable=False):
+        super().__init__(parent)
+
+        self.changeable = changeable
+        self.path = ""  # Used to store path to the currently-displayed file
+
+        self.setStyleSheet("background-color: white;")
+        self.setMinimumSize(512, 200)  # resize to Froggy boot logo dimensions
+
+        if self.changeable:
+            self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            self.setText("Click to Select New Image")
+
+    def mousePressEvent(self, ev):
+        """
+        Overrides built-in function to handle mouse click events. Prompts user for image path and loads same.
+        """
+        if self.changeable:  # only do something if image is changeable
+            file_name = QFileDialog.getOpenFileName(self, 'Open file', '',
+                                                    "Images (*.jpg *.png *.webp);;RAW (RGB565 Little Endian) Images (*.raw)")[0]
+            if len(file_name) > 0:  # confirm if user selected a file
+                self.load_image(file_name)
+
+    def load_from_bios(self, drive: str):
+        """
+        Extracts image from the bios and passes to load image function.
+
+        Args:
+            drive (str):  Path to the root of the Froggy drive.
+        """
+        with open(os.path.join(drive, "bios", "bisrv.asd"), "rb") as bios_file:
+            bios_content = bytearray(bios_file.read())
+
+        offset = tadpole_functions.findSequence(tadpole_functions.offset_logo_presequence, bios_content) + 16
+        with open(os.path.join(basedir, "bios_image.raw"), "wb") as image_file:
+            image_file.write(bios_content[offset:offset+((512*200)*2)])
+
+        self.load_image(os.path.join(basedir, "bios_image.raw"))
+
+    def load_image(self, path: str) -> bool:
+        """
+        Loads an image into the viewer.  If the image is loaded successfully, may enable the parent Save button based
+        on the changeable flag.
+
+        Args:
+            path (str): Path to the image.  Can be .raw or other format.  If .raw, assumed to be in RGB16 (RGB565 Little
+                Endian) format used for Froggy boot logos.  Must be 512x200 pixels or it will not be accepted/displayed.
+
+        Returns:
+            bool: True if image was loaded, False if not.
+        """
+        if os.path.splitext(path)[1] == ".raw":  # if raw image, assume RGB16 (RGB565 Little Endian)
+            with open(path, "rb") as f:
+                img = QImage(f.read(), 512, 200, QImage.Format_RGB16)
+        else:  # otherwise let QImage autodetection do its thing
+            img = QImage(path)
+            if (img.width(), img.height()) != (512, 200):  # check to determine if image is appropriate size
+                self.setText("Image dimensions are wrong.  Should be 512x200.  Click to try again.")
+                self.parent().button_save.setDisabled(True)  # disable saving
+                return False
+        self.path = path  # update path
+        self.setPixmap(QPixmap().fromImage(img))
+
+        if self.changeable:  # only enable saving for changeable dialogs; prevents enabling with load from bios
+            self.parent().button_save.setDisabled(False)
+        return True
+
+
+class BootConfirmDialog(QDialog):
+    """
+    Dialog used to confirm boot logo selection with the ability to view existing selection and replacement.
+
+    Args:
+        drive (str): Path to root of froggy drive.
+    """
+    def __init__(self, drive):
+        super().__init__()
+
+        self.drive = drive
+
+        self.setWindowTitle("Boot Image Selection")
+        self.setWindowIcon(QIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_DesktopIcon)))
+
+        # Setup Main Layout
+        self.layout_main = QVBoxLayout()
+        self.setLayout(self.layout_main)
+
+        # set up current image viewer
+        self.layout_main.addWidget(QLabel("Current Image"))
+        self.current_viewer = BootLogoViewer(self)
+        self.layout_main.addWidget(self.current_viewer)
+
+        self.layout_main.addWidget(QLabel(" "))  # spacer
+
+        # set up new image viewer
+        self.layout_main.addWidget(QLabel("New Image"))
+        self.new_viewer = BootLogoViewer(self, changeable=True)
+        self.layout_main.addWidget(self.new_viewer)
+
+        # Main Buttons Layout (Save/Cancel)
+        self.layout_buttons = QHBoxLayout()
+        self.layout_main.addLayout(self.layout_buttons)
+
+        # Save Button
+        self.button_save = QPushButton("Save")
+        self.button_save.setDefault(True)
+        self.button_save.setDisabled(True)  # set disabled by default; need to wait for user to select new image
+        self.button_save.clicked.connect(self.accept)
+        self.layout_buttons.addWidget(self.button_save)
+
+        # Cancel Button
+        self.button_cancel = QPushButton("Cancel")
+        self.button_cancel.clicked.connect(self.reject)
+        self.layout_buttons.addWidget(self.button_cancel)
+
+        # Load Initial Image
+        self.current_viewer.load_from_bios(self.drive)
+
+
 class MusicConfirmDialog(QDialog):
     """Dialog used to confirm music selection with the ability to preview selection by listening to the music.
 
@@ -343,7 +469,7 @@ class MainWindow (QMainWindow):
 
         # OS Menu
         self.action_updateToV1_5  = QAction("V1.5", self, triggered=self.UpdatetoV1_5)                                                                              
-        self.action_changeBootLogo  = QAction("Change &Boot Logo", self, triggered=self.changeBootLogo)
+        self.action_changeBootLogo  = QAction("&Boot Logo", self, triggered=self.changeBootLogo)
         self.GBABIOSFix_action = QAction("&GBA BIOS Fix", self, triggered=self.GBABIOSFix)
         self.action_changeShortcuts = QAction("Change Game Shortcuts", self, triggered=self.changeGameShortcuts)
         self.action_removeShortcutLabels = QAction("Remove Shortcut Labels", self, triggered=self.removeShortcutLabels)
@@ -409,24 +535,25 @@ from tzlion on frogtool. Special thanks also goes to wikkiewikkie for many amazi
         QMessageBox.about(self, "GBA BIOS Fix", "BIOS successfully copied")
         
     def changeBootLogo(self):
-        drive = window.combobox_drive.currentText()
-        newLogoFileName = QFileDialog.getOpenFileName(self,
-                                                      'Open file',
-                                                      'c:\\',
-                                                      "Image files (*.jpg *.png *.webp);;All Files (*.*)")[0]
-        
-        print(f"user tried to load image: {newLogoFileName}")
-        if newLogoFileName is None or newLogoFileName == "":
-            print("user cancelled image select")
-            return
-        
-        try:
-            tadpole_functions.changeBootLogo(os.path.join(drive, "bios", "bisrv.asd"), newLogoFileName)
-        except tadpole_functions.Exception_InvalidPath:
-            QMessageBox.about(self, "Change Boot Logo", "An error occurred. Please ensure that you have the right \
-            drive selected and <i>bisrv.asd</i> exists in the <i>bios</i> folder")
-            return
-        QMessageBox.about(self, "Change Boot Logo", "Boot logo successfully changed")
+        dialog = BootConfirmDialog(window.combobox_drive.currentText())
+        change = dialog.exec()
+        if change:
+            newLogoFileName = dialog.new_viewer.path
+            print(f"user tried to load image: {newLogoFileName}")
+            if newLogoFileName is None or newLogoFileName == "":
+                print("user cancelled image select")
+                return
+
+            try:
+                tadpole_functions.changeBootLogo(os.path.join(window.combobox_drive.currentText(),
+                                                              "bios",
+                                                              "bisrv.asd"),
+                                                 newLogoFileName)
+            except tadpole_functions.Exception_InvalidPath:
+                QMessageBox.about(self, "Change Boot Logo", "An error occurred. Please ensure that you have the right \
+                drive selected and <i>bisrv.asd</i> exists in the <i>bios</i> folder")
+                return
+            QMessageBox.about(self, "Change Boot Logo", "Boot logo successfully changed")
       
     def changeGameShortcuts(self):
         drive = window.combobox_drive.currentText()
