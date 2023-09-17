251ddb83f354724a1f0ab6b397b38f1760367608
EricGoldsteinNz <EricGoldsteinNz@users.noreply.github.com>
2023-09-18 06:47:44 +1200

    Move BootLogoViewer to BootConfirmDialog

--- dialogs/BootConfirmDialog.py
+++ dialogs/BootConfirmDialog.py
@@ -2,6 +2,11 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+
+import tadpole_functions
+
+import os
+
 class BootConfirmDialog(QDialog):
     """
     Dialog used to confirm boot logo selection with the ability to view existing selection and replacement.
@@ -9,9 +14,11 @@ class BootConfirmDialog(QDialog):
     Args:
         drive (str): Path to root of froggy drive.
     """
-    def __init__(self, drive):
+    def __init__(self, drive, basedir):
         super().__init__()
 
+
+        self.basedir = basedir
         self.drive = drive
 
         self.setWindowTitle("Boot Image Selection")
@@ -23,14 +30,14 @@ class BootConfirmDialog(QDialog):
 
         # set up current image viewer
         self.layout_main.addWidget(QLabel("Current Image"))
-        self.current_viewer = BootLogoViewer(self)
+        self.current_viewer = BootLogoViewer(self, basedir)
         self.layout_main.addWidget(self.current_viewer)
 
         self.layout_main.addWidget(QLabel(" "))  # spacer
 
         # set up new image viewer
         self.layout_main.addWidget(QLabel("New Image"))
-        self.new_viewer = BootLogoViewer(self, changeable=True)
+        self.new_viewer = BootLogoViewer(self, basedir, changeable=True)
         self.layout_main.addWidget(self.new_viewer)
 
         # Main Buttons Layout (Save/Cancel)
@@ -51,3 +58,77 @@ class BootConfirmDialog(QDialog):
 
         # Load Initial Image
         self.current_viewer.load_from_bios(self.drive)
+
+
+class BootLogoViewer(QLabel):
+    """
+    Args:
+        parent (BootConfirmDialog): Parent widget. Used to enable/disable controls on parent.
+        changeable (bool): If True, will allow importing new image. If False, will just allow static display.
+    """
+    def __init__(self, parent, basedir, changeable=False):
+        super().__init__(parent)
+        
+        self.basedir = basedir
+        self.changeable = changeable
+        self.path = ""  # Used to store path to the currently-displayed file
+
+        self.setStyleSheet("background-color: white;")
+        self.setMinimumSize(512, 200)  # resize to Froggy boot logo dimensions
+
+        if self.changeable:
+            self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            self.setText("Click to Select New Image")
+
+    def mousePressEvent(self, ev):
+        """
+        Overrides built-in function to handle mouse click events. Prompts user for image path and loads same.
+        """
+        if self.changeable:  # only do something if image is changeable
+            file_name = QFileDialog.getOpenFileName(self, 'Open file', '',
+                                                    "Images (*.jpg *.png *.webp);;RAW (RGB565 Little Endian) Images (*.raw)")[0]
+            if len(file_name) > 0:  # confirm if user selected a file
+                self.load_image(file_name)
+
+    def load_from_bios(self, drive: str):
+        """
+        Extracts image from the bios and passes to load image function.
+
+        Args:
+            drive (str):  Path to the root of the Froggy drive.
+        """
+        with open(os.path.join(drive, "bios", "bisrv.asd"), "rb") as bios_file:
+            bios_content = bytearray(bios_file.read())
+
+        offset = tadpole_functions.findSequence(tadpole_functions.offset_logo_presequence, bios_content) + 16
+        # TODO switch this to be in memory
+        with open(os.path.join(self.basedir, "bios_image.raw"), "wb") as image_file:
+            image_file.write(bios_content[offset:offset+((512*200)*2)])
+
+        self.load_image(os.path.join(self.basedir, "bios_image.raw"))
+
+    def load_image(self, path: str) -> bool:
+        """
+        Loads an image into the viewer.  If the image is loaded successfully, may enable the parent Save button based
+        on the changeable flag.
+
+        Args:
+            path (str): Path to the image.  Can be .raw or other format.  If .raw, assumed to be in RGB16 (RGB565 Little
+                Endian) format used for Froggy boot logos.  Must be 512x200 pixels or it will not be accepted/displayed.
+
+        Returns:
+            bool: True if image was loaded, False if not.
+        """
+        if os.path.splitext(path)[1] == ".raw":  # if raw image, assume RGB16 (RGB565 Little Endian)
+            with open(path, "rb") as f:
+                img = QImage(f.read(), 512, 200, QImage.Format_RGB16)
+        else:  # otherwise let QImage autodetection do its thing
+            img = QImage(path)
+            if (img.width(), img.height()) != (512, 200): 
+                img = img.scaled(512, 200, Qt.IgnoreAspectRatio, Qt.SmoothTransformation) #Rescale new boot logo to correct size
+        self.path = path  # update path
+        self.setPixmap(QPixmap().fromImage(img))
+
+        if self.changeable:  # only enable saving for changeable dialogs; prevents enabling with load from bios
+            self.parent().button_save.setDisabled(False)
+        return True
